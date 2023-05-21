3eb29d7ae2c7dbfd718635b8989d9e41ff96f732
rajan chouhan <rajan.chouhan25@gmail.com>
2023-05-21 15:01:35 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const bodyparser= require("body-parser");
+const axios = require("axios");
+const app=express();
+const dotenv= require("dotenv");
+dotenv.config({path:"./config.env"})
+app.use(bodyparser.json());
+app.get('/',(req,res)=>{
+    console.log(req.body);
+    res.json({
+        status:"success"
+    })
+}
+)
+
+app.get("/webhook",(req,res)=>{
+    let mode =req.query["hub.mode"];
+    let challenge =req.query["hub.challenge"];
+    let token = req.query["hub.verify_token"];
+
+    if(mode&&token)
+    {
+        if(mode==="subscribe" && token === process.env.MYTOKEN)
+        res.status(200).send(challenge);
+
+    }
+    else
+    {
+        res.status(403);  // 403 is forbidden response
+    }
+
+    
+})
+
+app.post("/webhook", (req,res)=>{
+    let body_param=req.body;
+    console.log(body_param);
+    if(body_param.object)
+    {
+        if(body_param.entry &&
+           body_param.entry[0].changes &&
+           body_param.entry[0].changes[0].value.messages&&
+           body_param.entry[0].changes[0].value.messages[0])
+           {
+            let phone_no_id = req.body.entry[0].changes[0].value.metadata.phone_number_id;
+            let from = req.body.entry[0].changes[0].value.messages[0].from;
+            let msg_body =req.body.entry[0].changes[0].value.messages[0].text.body;
+
+            axios({
+                method:"POST",
+                url:"https://graph.facebook.com/v16.0/"+phone_no_id+"/messages?access_token="+process.env.TOKEN,
+                data:{
+                    messaging_product:"whatsapp",
+                    to:from,
+                    text:{
+                        body:"Hi... I am rajan's whatsapp bot"
+                    }
+                },
+                headers:{
+                    "Content-Type":"application/json"
+                }
+            })
+
+            res.sendStatus(200);
+
+           }
+           else
+           res.sendStatus(404); // 404 is not found
+    }
+
+})
+
+
+
+
+
+
+module.exports=app;
