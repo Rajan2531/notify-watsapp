c9d33303b371b0905eab11b504f97a1fd4fc9353
rajan chouhan <rajan.chouhan25@gmail.com>
2023-06-15 00:51:04 +0530

    fixed

--- app.js
+++ app.js
@@ -226,6 +226,7 @@ app.post("/webhook", (req,res)=>{
                        model:"text-davinci-003",
                        prompt:question,
                        temperature:0.6,
+                       max_tokens:300
                        
                    });
                    
